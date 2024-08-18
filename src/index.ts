56b4cc6a56b285441b8c714a20bfa0305e969e34
Szymon Wiszczuk <golota60@gmail.com>
2024-08-18 22:10:08 +0200

    add simple state transitions

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,22 @@ type Transitions<T extends Record<string, unknown>> = keyof T extends string
   ? {
       [K in keyof T as `${TransitionName<K>}To${TransitionName<keyof T>}`]?:
         | {
-            /** Any additional action you want to run automatically when nanomachine changes state */
-            action?: () => void;
+            /**
+             * Any additional action you want to run automatically when nanomachine changes state.
+             * You can influence the transiton via this function.
+             * If your transition should only be valid under a condition, you can `throw` here to stop the transition.
+             * Then, your exact error will be re-thrown when you call `transition`.
+             *
+             * @param {unknown} context - The context to pass when executing `action` function during the transition.
+             */
+            action?: (context: unknown) => void;
           }
         | true;
     }
   : never;
 
-interface CreateMachineArgs<T extends Record<string, unknown>> {
-  initialState: keyof T;
+interface CreateMachineArgs<T extends Record<S, unknown>, S extends string> {
+  initialState: S;
   possibleStates: T;
   /**
    * Allowed transitions list. The existence of the named transition means it's valid.
@@ -21,10 +28,46 @@ interface CreateMachineArgs<T extends Record<string, unknown>> {
   transitions: Transitions<T>;
 }
 
-export const createNanomachine = <T extends Record<string, unknown>>(
-  createParams: CreateMachineArgs<T>
+export const createNanomachine = <
+  T extends Record<S, unknown>,
+  S extends string
+>(
+  createParams: CreateMachineArgs<T, S>
 ) => {
-  const { initialState, possibleStates } = createParams;
+  const { initialState, possibleStates, transitions } = createParams;
+  let state = initialState;
+
+  const possibleStatesValues = Object.values(possibleStates);
+  const possibleStatesNames = Object.keys(possibleStates);
+  const transitionBase = (targetState: S) => {
+    if (!targetState)
+      throw new Error(
+        `\`targetState\` was not provided while attempting to transition from state "${state}".`
+      );
+    if (possibleStatesNames.includes(targetState)) {
+      throw new Error(
+        `\`targetState\` is not listed as a possible state to transition from state "${state}".`
+      );
+    }
+
+    state = targetState;
+  };
+
+  return {
+    /** Attempt to transition to a target state, throws on fail */
+    transition: transitionBase,
+    /** A non-throw version of `transition`. Returns `false` on fail. */
+    transitionSafe: (targetState: S) => {
+      let transitionResult;
+      try {
+        transitionResult = transitionBase(targetState);
+      } catch (err) {
+        transitionResult = err;
+      }
+
+      return transitionResult;
+    },
+  };
 };
 
 const exampleUsage = () => {
@@ -47,8 +90,8 @@ const exampleUsage = () => {
     },
     transitions: {
       InitToStarted: {
-        action: () => {
-          // Any additional stuff you want to happen during the transition
+        action: (context: unknown) => {
+          // Any additional stuff you want to happen during the transition. Can influence the result!
         },
       },
       StartedToLoading: true,
