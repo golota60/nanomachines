ed8c23c4e77387178fd46aab8a5ac3e35ded049c
Szymon Wiszczuk <golota60@gmail.com>
2024-08-18 21:01:18 +0200

    adjust example

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,22 @@ type TransitionName<T extends string> = Capitalize<Lowercase<T>>;
 
 type Transitions<T extends Record<string, unknown>> = keyof T extends string
   ? {
-      [K in keyof T as `${TransitionName<K>}To${TransitionName<keyof T>}`]?: {
-        /** Any additional action you want to run automatically when nanomachine changes state */
-        action?: () => void;
-      };
+      [K in keyof T as `${TransitionName<K>}To${TransitionName<keyof T>}`]?:
+        | {
+            /** Any additional action you want to run automatically when nanomachine changes state */
+            action?: () => void;
+          }
+        | true;
     }
   : never;
 
 interface CreateMachineArgs<T extends Record<string, unknown>> {
   initialState: keyof T;
   possibleStates: T;
+  /**
+   * Allowed transitions list. The existence of the named transition means it's valid.
+   * E.g. If transition `OneToTwo` doesn't exist, then trying to make that transition will error
+   */
   transitions: Transitions<T>;
 }
 
@@ -22,13 +28,22 @@ export const createNanomachine = <T extends Record<string, unknown>>(
 };
 
 const exampleUsage = () => {
+  enum PossibleStates {
+    INIT = "INIT",
+    STARTED = "STARTED",
+    LOADING = "LOADING",
+    FINISH = "FINISH",
+  }
+
   const machine = createNanomachine({
-    initialState: "INIT",
+    initialState: PossibleStates.INIT,
     possibleStates: {
-      INIT: {},
-      STARTED: {},
-      LOADING: {},
-      FINISH: {},
+      [PossibleStates.INIT]: {
+        // Store any additional context to be associated with the state
+      },
+      [PossibleStates.STARTED]: {},
+      [PossibleStates.LOADING]: {},
+      [PossibleStates.FINISH]: {},
     },
     transitions: {
       InitToStarted: {
@@ -36,6 +51,7 @@ const exampleUsage = () => {
           // Any additional stuff you want to happen during the transition
         },
       },
+      StartedToLoading: true,
     },
   });
 };
